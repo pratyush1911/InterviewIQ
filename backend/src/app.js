8e6f27b801bde61bc020e9f817f6381d7fd24386
Pratyush <pratyush12001@gmail.com>
2026-05-19 15:59:15 +0530

    fixed some cors bugs

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,7 +35,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/.*/", cors(corsOptions));
 
 const authRouter = require("./routes/auth.route");
 const interviewRouter = require("./routes/interview.routes");
